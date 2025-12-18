d15d458985aafbc93748f42e247d510b4987c381
TewogbolaFakuade <fakuadetewogbola@gmail.com>
2025-12-18 11:42:32 +0100

    Rename file to Tewo Exercise 1-3.py

---
+++ Tewo Exercise 1-3.py
@@ -0,0 +1,45 @@
+# Exercise 1 (A Good Program)
+print ("Hello World!")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun.")
+print('Yay! Printing.')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.') 
+
+
+#Exercise 2(Comments and pound characters)
+# A comment, this is so you can read your program later.
+# Anythingafterthe#isignoredbypython.
+
+print("I could have code like this.")#andthecommenta
+
+# You can also use a comment to "disable" or comment out an error.
+# print*(*"This won't run."*)*
+print("This will run.")
+
+
+# Exercise 3 (Numbers and Math)
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+ 
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater orequal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
