67dfc3b633c3540a83a394286a08a358a79b8e50
GideonPromise <promisebola2004@gmail.com>
2025-12-18 11:10:05 +0100

    Gideon-Exercise 10-12.py

---
+++ Gideon-Exercise 10-12.py
@@ -0,0 +1,25 @@
+# Exercise 10. Escape Codes in Strings
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+
+# Exercise 11. Asking People Questions
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+print(f"So, you're {age} old, {height} tall and {weight} heavy.)
